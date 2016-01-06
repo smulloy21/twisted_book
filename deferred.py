65b84afc0ff55dcaca7420e4da55073914179e0b
Shannon Mulloy <smulloy21@gmail.com>
2016-01-06 13:13:32 -0800

    practice with deferreds

--- deferred.py
+++ deferred.py
@@ -1,18 +1,73 @@
 from twisted.internet.defer import Deferred
 
-def addBold(msg):
-    return "<b>%s</b>" % (msg,)
+def callback1(result):
+    print "Callback 1 said:", result
+    return result
 
-def addItalic(msg):
-    return "<i>%s</i>" % (msg,)
+def callback2(result):
+    print "Callback 2 said:", result
 
-def printHTML(msg):
-    print msg
+def callback3(result):
+    raise Exception("Callback 3")
 
+def errback1(failure):
+    print "Errback 1 had an error on", failure
+    return failure
+
+def errback2(failure):
+    raise Exception("Errback 2")
+
+def errback3(failure):
+    print "Errback 3 took care of", failure
+    return "Everything is fine now."
+
+# Exercise 1
+# d = Deferred()
+# d.addCallback(callback1)
+# d.addCallback(callback2)
+# d.callback("Test")
+
+# Exercise 2
+# d = Deferred()
+# d.addCallback(callback1)
+# d.addCallback(callback2)
+# d.addCallback(callback3)
+# d.callback("Test")
+
+# Exercise 3
+# d = Deferred()
+# d.addCallback(callback1)
+# d.addCallback(callback2)
+# d.addCallback(callback3)
+# d.addErrback(errback3)
+# d.callback("Test")
+
+# Exercise 4
+# d = Deferred()
+# d.addErrback(errback1)
+# d.errback(Exception("Test"))
+
+# Exercise 5
+# d = Deferred()
+# d.addErrback(errback1)
+# d.addErrback(errback3)
+# d.errback(Exception("Test"))
+
+# Exercise 6
+# d = Deferred()
+# d.addErrback(errback2)
+# d.errback(Exception("Test"))
+
+# Exercise 7
+# d = Deferred()
+# d.addCallback(callback1)
+# d.addCallback(callback2)
+# d.addCallbacks(callback3, errback3)
+# d.callback("Test")
+
+# Exercise 8
 d = Deferred()
-d.addCallback(addItalic)
-d.addCallback(addBold)
-
-d.addCallback(printHTML)
-
-d.callback('Yo.')
+d.addCallback(callback3)
+d.addCallbacks(callback2, errback3)
+d.addCallbacks(callback1, errback2)
+d.callback("Test")
