a6ec80551177f442e9aa1f1f5e9e25866ff67644
Shannon Mulloy <smulloy21@gmail.com>
2016-01-06 15:03:16 -0800

    seventh example: serve static web content file tree

--- static_content.py
+++ static_content.py
@@ -1,10 +1,9 @@
 from twisted.internet import reactor
-from twisted.web.resource import Resource
 from twisted.web.server import Site
 from twisted.web.static import File
 
-root = Resource()
-root.putChild('hello', File('/twisted_book/hello'))
+root = File('/')
+root.putChild('var', File('/var'))
 factory = Site(root)
 reactor.listenTCP(8000, factory)
 reactor.run()
